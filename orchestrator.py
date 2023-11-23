c8b462edb5dc2dd206bdcecf3a70270d2ba5d682
Arash Mirshahi <123241327+Arashi20@users.noreply.github.com>
2023-11-23 11:08:10 +0100

    Update orchestrator.py
    This is the new orchestrator that should work with AWS SQS

--- orchestrator.py
+++ orchestrator.py
@@ -2,47 +2,80 @@ from flask import Flask, request, jsonify
 from queue import Queue
 from threading import Thread
 from summarization_model import sumArticle2
+from transformers import pipeline
+import boto3
+import json
+
+
+
+sqs_queue_url = 'https://sqs.eu-north-1.amazonaws.com/951932431518/Summarticle.fifo'
+
+# Create an SQS client
+sqs = boto3.client('sqs')
+
 
 app = Flask(__name__)
 
-# Define the Flask app.
+
 
 # Create an endpoint for submitting summarization tasks.
 @app.route('/submit_summary', methods=['POST'])
 def submit_summary_task():
     data = request.get_json()  # JSON data with the text to summarize.
-    
-    
-    # Call the summarization function from your summarization model code.
-    summary = sumArticle2(data['pdf_path'])
 
-    return jsonify({'message': 'Summary task submitted successfully', 'summary' : summary})
+    # Send message to SQS queue with the entire PDF content as the message body
+    response = sqs.send_message(
+        QueueUrl=sqs_queue_url,
+        MessageBody=data['pdf_content'],  # Assuming 'pdf_content' is the key for the PDF content
+        MessageGroupId='1'  # Specify the message group ID
+    )
 
-# Set up a task queue and worker thread for summarization.
-summary_queue = Queue()
+    return jsonify({'message': 'Summary task submitted successfully', 'summary': response['MessageId']})
+
 
 def summary_worker():
     while True:
-        task_data = summary_queue.get()
-        # Process the summarization task using your logic.
-        # You can call your summarization function here.
-        # Task handling logic for summarization goes here.
-        print("Summarizing:", task_data)
-        summary_queue.task_done()
+        # Receive message from SQS queue
+        response = sqs.receive_message(
+            QueueUrl=sqs_queue_url,
+            AttributeNames=['All'],
+            MessageAttributeNames=['All'],
+            MaxNumberOfMessages=1,
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
 
-summary_worker_thread = Thread(target=summary_worker)
-summary_worker_thread.daemon = True
-summary_worker_thread.start()
+        # Process the received message
+        if 'Messages' in response:
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
+
+            # Process the message data safely using json.loads
+            try:
+                pdf_content = message['Body']
+                summary = sumArticle2(pdf_content)
+
+                # Add your logic for processing the summarized data
+                print("Summarizing:", summary)
+            except Exception as e:
+                print(f"Error processing PDF content: {e}")
+
+            # Delete received message from queue
+            sqs.delete_message(
+                QueueUrl=sqs_queue_url,
+                ReceiptHandle=receipt_handle
+            )
+
 
 # Create an endpoint for submitting translation tasks.
 @app.route('/submit_translation', methods=['POST'])
 def submit_translation_task():
     data = request.get_json()  # JSON data with the text to translate and target language.
     
-    # Enqueue the translation task.
-    translation_queue.put(data)  # Assuming 'data' contains the text and target language.
+    # Call the translation function.
+    translation = translate_text(data['text'], data['target_language'])
 
-    return jsonify({'message': 'Translation task submitted successfully'})
+    return jsonify({'message': 'Translation task submitted successfully', 'translation': translation})
 
 # Set up a task queue and worker thread for translation.
 translation_queue = Queue()
@@ -85,7 +118,12 @@ recommend_worker_thread = Thread(target=recommend_worker)
 recommend_worker_thread.daemon = True
 recommend_worker_thread.start()
 
+# Translation function using transformers pipeline.
+def translate_text(text, target_language):
+    translation_pipeline = pipeline(task="translation", model="Helsinki-NLP/opus-mt-en-nl")
+    translation_result = translation_pipeline(text, target_lang=target_language)
+    return translation_result[0]['translation_text']
+
 # Run the Flask app.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
-
